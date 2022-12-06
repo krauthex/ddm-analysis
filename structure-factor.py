c3e38daecef2175a990af02f82af57cbf6569e49
Fabian Krautgasser <fabian.krautgasser@zamg.ac.at>
2022-12-06 17:29:43 +0100

    DEV: multicore evaluation of cell areas

--- structure-factor.py
+++ structure-factor.py
@@ -2,8 +2,10 @@
 """Perform real space analysis of fluorescence cell nuclei to get the structure factor."""
 
 import argparse
+from multiprocessing import Pool
 from pathlib import Path
-from typing import Any, Dict
+from time import perf_counter
+from typing import Any, Callable, Dict, Sequence
 
 import numpy as np
 from csbdeep.utils import normalize
@@ -35,6 +37,19 @@ def images(path: Path) -> np.ndarray:
     return tiff_to_numpy(path)
 
 
+def multicore_dispatch(
+    func: Callable,
+    func_iterable: Sequence[Any],
+    *,
+    cpus: int = 2,
+) -> Any:
+    pool = Pool(cpus)
+    chunksize = len(func_iterable) // cpus  # rough chunksize setting
+    results = pool.imap(func, func_iterable, chunksize=chunksize)
+
+    return results
+
+
 def area(coord: np.ndarray) -> float:
     points = [Point(*pos) for pos in coord.T]
     polygon = Polygon(*points)
@@ -49,21 +64,29 @@ def stats(details: Details) -> Any:
 
 parser = argparse.ArgumentParser()
 parser.add_argument("src", help="The source TIFF file.")
+parser.add_argument(
+    "--cpus", type=int, default=1, help="Number of CPUs to use for polygon computation."
+)
 
 args = parser.parse_args()
 
 if __name__ == "__main__":
+    # timing
+    start = perf_counter()
+
     # creates a pretrained model
     model = StarDist2D.from_pretrained("2D_versatile_fluo")
     img = images(args.src)
+    stardist_time = perf_counter()
     l, d = stardist_single(img[0], model)
+    stardist_time = perf_counter() - stardist_time
+    print(f":: Stardist took {stardist_time:.2f}s")
     coord = d["coord"]
-    print(len(coord))
-    N = 100
+    N = len(coord)
     areas = np.zeros(N)
-    for i, verts in enumerate(coord[:N]):
-        print("calculating polygon: ", i)
-        areas[i] = area(verts)
+    for i, result in enumerate(multicore_dispatch(area, coord[:N], cpus=args.cpus)):
+        areas[i] = result
 
     print(f"Average area: {areas.mean()} \pm {areas.std()/np.sqrt(N)}")
+    print(f":: Overall runtime: {perf_counter()-start:.2f}s")
     # keys: coord, points, prob
